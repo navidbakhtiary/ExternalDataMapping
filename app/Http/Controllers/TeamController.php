a7db12277567b456cb7cf7bf1b0a556b4e6da06f
navidbakhtiary <navidbakhtiary@yahoo.com>
2022-06-19 12:38:11 +0430

    fix: make some changes in TeamController for extract data, create collection and save into database

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,38 +2,73 @@
 
 namespace App\Http\Controllers;
 
-use App\Classes\TeamJsonResponse;
-use App\Classes\TeamXMLResponse;
-use Illuminate\Http\Request;
+use App\Classes\TeamJsonData;
+use App\Classes\TeamsJsonCollection;
+use App\Classes\TeamsXMLCollection;
+use App\Classes\TeamXMLData;
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Yaml;
 
 class TeamController extends Controller
 {
     public function jsonIndex()
     {
-        $response = Http::get(config('global.ext_api.teams.url'));
-
-        // dd($response->body(), $response->object(), $response->json());
-        $json_response = new TeamJsonResponse($response);
-        $json_response->extractData();
-        dd($json_response->data);
-        // {
-        //     dd($json_response->data);
-        // }
-        return '';
+        try
+        {
+            //Get response from external API and extract json from it
+            $response = Http::get(config('global.ext_api.teams.url'));
+            $json_response = new TeamJsonData($response);
+            $json_response->extractData();
+            
+            //Parse YAML file and get json mapper from it
+            $mapper = Yaml::parseFile(config_path('mappers/team.yaml'))['json'];
+            
+            //Get teams data and save into database
+            $collection = new TeamsJsonCollection($json_response, $mapper);
+            $collection->create();
+            DB::beginTransaction();
+            foreach($collection->teams as $team)
+            {
+                $team->save();
+            }
+            DB::commit();
+            return 'successful';
+        }
+        catch(Exception $exc)
+        {
+            DB::rollBack();
+            return 'failed';
+        }
     }
 
     public function xmlIndex()
     {
-        $response = Http::get(config('global.ext_api.teams.url') . '.xml');
-        // $bodbod = simplexml_load_string($response->body());
-        // dd($bodbod, property_exists($bodbod, 'data'));
-        $xml_response = new TeamXMLResponse($response);
-        $xml_response->extractData();
-        dd($xml_response->data); 
-        // {
-        //     dd($xml_response->data);
-        // }
-        return 'error xml';
+        try
+        {
+            //Get response from external API and extract xml from it
+            $response = Http::get(config('global.ext_api.teams.url') . '.xml');
+            $xml_response = new TeamXMLData($response);
+            $xml_response->extractData();
+
+            //Parse YAML file and get xml mapper from it
+            $mapper = Yaml::parseFile(config_path('mappers/team.yaml'))['xml'];
+
+            //Get teams data and save into database
+            $collection = new TeamsXMLCollection($xml_response, $mapper);
+            $collection->create();
+            DB::beginTransaction();
+            foreach ($collection->teams as $team) {
+                $team->save();
+            }
+            DB::commit();
+            return 'successful';
+        } 
+        catch (Exception $exc) 
+        {
+            DB::rollBack();
+            return 'failed';
+        }
     }
 }
