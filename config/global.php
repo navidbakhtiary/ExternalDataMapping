ac30fddb0a590480267a6938e554a6e15e0c1376
navidbakhtiary <navidbakhtiary@yahoo.com>
2022-06-19 11:43:50 +0430

    feat: symfony/yaml package has been added by composer for parsing yaml file

--- config/global.php
+++ config/global.php
@@ -2,10 +2,6 @@
 
 return [
     'ext_api' => [
-        'countries' => [
-            'url' => 'https://api.first.org/data/v1/countries',
-            'data_key' => 'data'
-        ],
         'teams' => [
             'url' => 'https://api.first.org/data/v1/teams',
             'data_key' => 'data'
